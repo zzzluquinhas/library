06934e138ae256c9f3c977fc82583fc246a2e30e
Lucas Almeida <72702201+zzzluquinhas@users.noreply.github.com>
2025-09-25 22:04:06 -0300

    Commit 6: Moving method get_charge from Rental to Book

--- model.py
+++ model.py
@@ -7,6 +7,21 @@ class Book:
     def __init__(self, title: str, price_code: int):
         self.title = title
         self.price_code = price_code
+    
+    def get_charge(self, days_rented: int) -> float:
+        amount = 0
+        if self.price_code == self.REGULAR:
+            amount += 2
+            if days_rented > 2:
+                amount += (days_rented - 2) * 1.5
+        elif self.price_code == self.NEW_RELEASE:
+            amount += days_rented * 3
+        elif self.price_code == self.CHILDREN:
+            amount += 1.5
+            if days_rented > 3:
+                amount += (days_rented - 3) * 1.5
+
+        return amount
 
 class Rental:
     def __init__(self, book: Book, days_rented: int):
@@ -14,19 +29,7 @@ class Rental:
         self.days_rented = days_rented
 
     def get_charge(self) -> float:
-        amount = 0
-        if self.book.price_code == Book.REGULAR:
-            amount += 2
-            if self.days_rented > 2:
-                amount += (self.days_rented - 2) * 1.5
-        elif self.book.price_code == Book.NEW_RELEASE:
-            amount += self.days_rented * 3
-        elif self.book.price_code == Book.CHILDREN:
-            amount += 1.5
-            if self.days_rented > 3:
-                amount += (self.days_rented - 3) * 1.5
-
-        return amount
+        return self.book.get_charge(self.days_rented)
     
     def get_frequent_renter_points(self):
         points = 1
